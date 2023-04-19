e3d21e283d89333a52f75c3eed00a833d039dd66
Armin Zoerkler <armin.zoerkler@gmail.com>
2023-04-19 15:25:08 +0200

    Added quantile tests and renamed attributes

--- analyzer/toolbox.py
+++ analyzer/toolbox.py
@@ -8,37 +8,37 @@ class Toolbox:
         '''
         Calculate mean of a data series
         '''
-        self.mean = np.mean(data)
+        self._mean = np.mean(data)
 
     def std(self, data):
         '''
         Calculate sample standard deviation of a data series
         '''
-        self.std = np.std(data, ddof = 1)
+        self._std = np.std(data, ddof = 1)
 
     def median(self, data):
         '''
         Calculate median of a data series
         '''
-        self.median = np.median(data)
+        self._median = np.median(data)
 
     def quantile(self, data, n):
         '''
         Calculate quantile n of a data series,
         e.g. n = 4 calculates quartile, n = 5 calculates quintile etc.
         '''
-        if not hasattr(self, "quantile"):
-            self.quantile = {}
-        self.quantile[n] = np.quantile(data, q = 100 / n)
+        if not hasattr(self, "_quantile"):
+            self._quantile = {}
+        self._quantile[n] = np.quantile(data, q = 1 / n)
 
     def percentile(self, data, n):
         '''
         Calculate percentile of n percent of a data series,
         e.g. n = 25 calculates quartile, n = 75 calculates three quartiles etc.
         '''
-        if not hasattr(self, "percentile"):
-            self.percentile = {}
-        self.percentile[n] = np.percentile(data, q = n)
+        if not hasattr(self, "_percentile"):
+            self._percentile = {}
+        self._percentile[n] = np.percentile(data, q = n)
 
     def iqr(self, data):
         '''
@@ -46,8 +46,8 @@ class Toolbox:
         (difference between percentile of 75 percent and percentile of 25 percent)
         '''
         try:
-            self.iqr = self.percentile[75] - self.percentile[25]
+            self._iqr = self._percentile[75] - self._percentile[25]
         except(AttributeError, KeyError):
-            self.percentile(self, data, n = 25)
-            self.percentile(self, data, n = 75)
-            self.iqr(self, data)
+            self._percentile(self, data, n = 25)
+            self._percentile(self, data, n = 75)
+            self._iqr(self, data)
--- tests/test_toolbox.py
+++ tests/test_toolbox.py
@@ -6,21 +6,45 @@ test_data = [0, 1, 2, 3, 4, 4.2, 1.1, 3.2, 9.7, 5.5, 14.7, 15.8]
 
 def test_mean():
     test_toolbox.mean(test_data)
-    assert(test_toolbox.mean == pytest.approx(5.35))
+    assert(test_toolbox._mean == pytest.approx(5.35))
 
 def test_std():
     test_toolbox.std(test_data)
-    assert(round(test_toolbox.std,4) == pytest.approx(5.2716))
+    assert(round(test_toolbox._std,4) == pytest.approx(5.2716))
 
 def test_median():
     test_toolbox.median(test_data)
-    assert(test_toolbox.median == pytest.approx(3.6))
+    assert(test_toolbox._median == pytest.approx(3.6))
 
 def test_quantile_nothasattr():
-    pass
+    if hasattr(test_toolbox, "_quantile"):
+        del test_toolbox._quantile
+
+    n = 4
+
+    test_toolbox.quantile(test_data, n)
+    assert(test_toolbox._quantile[n] == pytest.approx(1.775))
+    del test_toolbox._quantile
+
+    n = 5
+
+    test_toolbox.quantile(test_data, n)
+    assert(test_toolbox._quantile[n] == pytest.approx(1.28))
+
 
 def test_quantile_hasattr():
-    pass
+    if not hasattr(test_toolbox, "_quantile"):
+        test_toolbox._quantile = {}
+
+    n = 5
+
+    test_toolbox.quantile(test_data, n)
+    assert (test_toolbox._quantile[n] == pytest.approx(1.28))
+
+    n = 4
+
+    test_toolbox.quantile(test_data, n)
+    assert (test_toolbox._quantile[n] == pytest.approx(1.775))
 
 def test_percentile_nothasattr():
     pass
